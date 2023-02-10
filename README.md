04629062d78c1bad4371b81ff03c65805f25e64a
niklas389 <niklasxwagner@outlook.com>
2023-02-10 18:16:27 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# Modpack for Nameless on Android 13
+
+[> Download <](https://github.com/niklas389/kb2003_R_modpack/releases/latest)
+
+## Features
+
+- iOS 16 Emojis
+- Some Google Play Services will be disabled on boot (see below)
+- H²OS Bootanimation
+- Disabled unnecessary OnePlus & Google Apps
+- Multiuser will be disabled automatically (Can be enabled in Settings)
+- Blurred Background (Notification shade)
+
+### Disabled Google Play Services functions
+
+- com.google.android.gms.analytics.service.AnalyticsService
+- com.google.android.gms.nearby.discovery.service.DiscoveryService
+- com.google.android.gms.ads.AdRequestBrokerService
+- com.google.android.gms.checkin.CheckinApiService
+- com.google.android.gms.ads.identifier.service.AdvertisingIdService
+  - [You won't get an Google Advertising Id after installing this mod - may break apps/games!]
---
+++ common/service.sh
@@ -0,0 +1,27 @@
+#!/system/bin/sh
+# Do NOT assume where your module will be located.
+# ALWAYS use $MODDIR if you need to know where this script
+# and module is placed.
+# This will make sure your module will still work
+# if Magisk change its mount point in the future
+MODDIR=${0%/*}
+
+# This script will be executed in late_start service mode
+
+# Late props which must be set after boot_completed
+while [ "$(getprop sys.boot_completed)" != "1" ]; do
+  sleep 1
+done
+# Disabling services
+pm disable com.google.android.gms/com.google.android.gms.analytics.service.AnalyticsService
+pm disable com.google.android.gms/com.google.android.gms.nearby.discovery.service.DiscoveryService
+pm disable com.google.android.gms/com.google.android.gms.ads.AdRequestBrokerService
+pm disable com.google.android.gms/com.google.android.gms.checkin.CheckinApiService
+pm disable com.google.android.gms/com.google.android.gms.ads.identifier.service.AdvertisingIdService
+pm disable com.google.android.gms/com.google.android.gms.analytics.AnalyticsService
+pm disable com.google.android.gms/com.google.android.gms.measurement.AppMeasurementJobService
+pm disable com.google.android.gms/com.google.android.gms.measurement.AppMeasurementInstallReferrerReceiver
+pm disable com.google.android.gms/com.google.android.gms.measurement.PackageMeasurementReceiver
+pm disable com.google.android.gms/com.google.android.gms.measurement.PackageMeasurementService
+pm disable com.google.android.gms/com.google.android.gms.measurement.service.MeasurementBrokerService
+pm disable com.google.android.gms/com.google.android.location.internal.AnalyticsSamplerReceiver
